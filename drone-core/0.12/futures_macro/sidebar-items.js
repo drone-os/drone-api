236ac9e380f4a4890855d0a174c392002c279d15
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2020-05-13 21:13:19 +0300

    Docs for drone-core/0.12

--- drone-core/0.12/futures_macro/sidebar-items.js
+++ drone-core/0.12/futures_macro/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"derive":[["proc_macro_hack_join","The `join!` macro."],["proc_macro_hack_select","The `select!` macro."],["proc_macro_hack_select_biased","The `select_biased!` macro."],["proc_macro_hack_try_join","The `try_join!` macro."]]});
+initSidebarItems({"derive":[["proc_macro_hack_join_internal","The `join!` macro."],["proc_macro_hack_select_biased_internal","The `select_biased!` macro."],["proc_macro_hack_select_internal","The `select!` macro."],["proc_macro_hack_try_join_internal","The `try_join!` macro."]]});
