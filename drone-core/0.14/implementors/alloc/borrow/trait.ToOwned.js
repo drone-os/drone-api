8605811a4d6ab8378b76352cbe79d476a4144868
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2021-04-09 15:42:53 +0300

    Docs for drone-core/0.14

---
+++ drone-core/0.14/implementors/alloc/borrow/trait.ToOwned.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["drone_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"drone_core/ffi/struct.CStr.html\" title=\"struct drone_core::ffi::CStr\">CStr</a>","synthetic":false,"types":["drone_core::ffi::c_str::CStr"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
